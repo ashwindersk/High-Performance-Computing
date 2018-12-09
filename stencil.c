50ea2575895d0b104be28f417490847bfc28a38d
Ashwinder Khurana <ashwinderk@live.co.uk>
2018-12-09 13:43:43 +0000

    added ;

--- stencil.c
+++ stencil.c
@@ -104,7 +104,7 @@ void stencil(const int nx, const int ny,  float *restrict image, float *restrict
   // else if( rank >0 && rank < 15){  
 
   printf("tester value : %d", tester)
-  MPI_Finalize()
+  MPI_Finalize();
   
   
 
