8504630e4c70ba0abe292a75c900398749ac3c8a
Ashwinder Khurana <ashwinderk@live.co.uk>
2018-12-09 15:32:26 +0000

    add print args

--- stencil.c
+++ stencil.c
@@ -61,7 +61,7 @@ int main(int argc, char *argv[]) {
   
   MPI_Scatter(image, sectionSize, MPI_FLOAT, bufferImg, sectionSize, MPI_FLOAT, 0, MPI_COMM_WORLD );
   
-  printf("Current rank is %d and first value of buffer is %f", bufferImg[0] );
+  printf("Current rank is %d and first value of buffer is %f", rank,bufferImg[0] );
 
 
 
