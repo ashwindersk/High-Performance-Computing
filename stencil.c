bc69780abeaf57572b1f541eda1535e647f27871
Ashwinder Khurana <ashwinderk@live.co.uk>
2018-12-09 12:25:36 +0000

    for loop tester

--- stencil.c
+++ stencil.c
@@ -85,7 +85,9 @@ void stencil(const int nx, const int ny,  float *restrict image, float *restrict
   int size = ny*nx/16;
   float* buffer=NULL;
   int *sixteen = malloc(16*sizeof(int));
-  sixteen = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+  for (int i =0 ; i< 16 ; i++){
+    sixteen[i] = i;
+  }
   int * tester =NULL;
 
   int root =0 ;
