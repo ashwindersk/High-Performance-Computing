ad412b9e19c06722edd9aaeebbb869ab8311a96e
Ashwinder Khurana <ashwinderk@live.co.uk>
2018-12-10 12:48:55 +0000

    db

--- stencil.c
+++ stencil.c
@@ -63,7 +63,7 @@ int main(int argc, char *argv[])
   for (int t = 0; t <  niters; ++t)
   { 
     stencil(nx, ny / 16, bufferImg, bufferTempImg, rank);
-    //stencil(nx, ny / 16, bufferTempImg, bufferImg, rank);
+    stencil(nx, ny / 16, bufferTempImg, bufferImg, rank);
   }
  
   double toc = wtime();
