1b828faff9207978764eb28b0f698097771775a9
Ashwinder Khurana <ashwinderk@live.co.uk>
2018-12-10 13:39:18 +0000

    db

--- stencil.c
+++ stencil.c
@@ -138,7 +138,7 @@ void stencil(const int nx, const int ny, float *restrict image, float *restrict 
 
     //MPI_Sendrecv( firstRowSend , nx, MPI_FLOAT, rank - 1, 0 , firstRowRecv , nx, MPI_FLOAT, rank-1, 0, MPI_COMM_WORLD, status);
     printf("deadlock \n");
-    MPI_Sendrecv( lastRowSend , nx, MPI_FLOAT, rank + 14, 0 , lastRowRecv , nx, MPI_FLOAT, rank+14, 0, MPI_COMM_WORLD, status);
+    MPI_Sendrecv( lastRowSend , nx, MPI_FLOAT, rank + 14, 15 , lastRowRecv , nx, MPI_FLOAT, rank+14, 15, MPI_COMM_WORLD, status);
     //MPI_Sendrecv( lastRowSend , nx, MPI_FLOAT, 15, 0 , lastRowRecv , nx, MPI_FLOAT, 15, 0, MPI_COMM_WORLD, status);
   
     
@@ -163,7 +163,7 @@ void stencil(const int nx, const int ny, float *restrict image, float *restrict 
 
     MPI_Status *status;
   
-    MPI_Sendrecv(firstRowSend, nx, MPI_FLOAT, 1, 0, firstRowRecv, nx, MPI_FLOAT, 1, 0, MPI_COMM_WORLD, status);
+    MPI_Sendrecv(firstRowSend, nx, MPI_FLOAT, 1, 15, firstRowRecv, nx, MPI_FLOAT, 1, 15, MPI_COMM_WORLD, status);
    
     free(firstRowSend);
     free(firstRowRecv);
