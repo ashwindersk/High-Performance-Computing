7b6869346b551e8586df3cca7de36f36fc1c23a1
Ashwinder Khurana <ashwinderk@live.co.uk>
2018-12-10 13:05:14 +0000

    db

--- stencil.c
+++ stencil.c
@@ -113,7 +113,7 @@ void stencil(const int nx, const int ny, float *restrict image, float *restrict 
     free(lastRowSend);
     free(lastRowRecv);
   }
-   else if (rank > 0 && rank < 15)
+   else if (rank==1)
   {
     
     float *firstRowRecv = (float *) malloc(nx * sizeof(float));
@@ -150,20 +150,20 @@ void stencil(const int nx, const int ny, float *restrict image, float *restrict 
   {
 
     //sending the first row of the array to rank 14;
-    int start = 0;
-    int end = nx - 1;
+    // int start = 0;
+    // int end = nx - 1;
 
-    float *firstRowSend = malloc(nx * sizeof(float));
-    firstRowSend = extractElements(firstRowSend, image, start, end);
+    // float *firstRowSend = malloc(nx * sizeof(float));
+    // firstRowSend = extractElements(firstRowSend, image, start, end);
 
-    float *firstRowRecv = malloc(nx * sizeof(float));
+    // float *firstRowRecv = malloc(nx * sizeof(float));
 
-    MPI_Status *status;
+    // MPI_Status *status;
   
-    MPI_Sendrecv(firstRowSend, nx, MPI_FLOAT, rank - 1, 0, firstRowRecv, nx, MPI_FLOAT, rank - 1, 0, MPI_COMM_WORLD, status);
+    // MPI_Sendrecv(firstRowSend, nx, MPI_FLOAT, rank - 1, 0, firstRowRecv, nx, MPI_FLOAT, rank - 1, 0, MPI_COMM_WORLD, status);
    
-    free(firstRowSend);
-    free(firstRowRecv);
+    // free(firstRowSend);
+    // free(firstRowRecv);
     
   }
   
