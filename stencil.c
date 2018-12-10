b5839d3ef07cb11189193bd37b05b73af944f6ae
Ashwinder Khurana <ashwinderk@live.co.uk>
2018-12-10 16:17:19 +0000

    db

--- stencil.c
+++ stencil.c
@@ -137,10 +137,10 @@ void stencil(const int nx, const int ny, float *restrict image, float *restrict 
     MPI_Status *status;
   
 
-    MPI_Sendrecv( firstRowSend , nx, MPI_FLOAT, rank - 1, 0 , firstRowRecv , nx, MPI_FLOAT, rank-1, 0, MPI_COMM_WORLD, status);
+    //MPI_Sendrecv( firstRowSend , nx, MPI_FLOAT, rank - 1, 0 , firstRowRecv , nx, MPI_FLOAT, rank-1, 0, MPI_COMM_WORLD, status);
     printf("deadlock \n");
     //MPI_Sendrecv( lastRowSend , nx, MPI_FLOAT, rank + 14, 15 , lastRowRecv , nx, MPI_FLOAT, rank+14, 15, MPI_COMM_WORLD, status);
-   // MPI_Sendrecv( lastRowSend , nx, MPI_FLOAT, 15, 0 , lastRowRecv , nx, MPI_FLOAT, 15, 0, MPI_COMM_WORLD, status);
+    MPI_Sendrecv( lastRowSend , nx, MPI_FLOAT, 2, 0 , lastRowRecv , nx, MPI_FLOAT, 2, 0, MPI_COMM_WORLD, status);
     printf("finish 1\n");
   
     
@@ -151,7 +151,7 @@ void stencil(const int nx, const int ny, float *restrict image, float *restrict 
     free(lastRowSend);
 
   }
-  else if(rank == 15)
+  else if(rank == 2)
   {
 
     //sending the first row of the array to rank 14
@@ -165,7 +165,7 @@ void stencil(const int nx, const int ny, float *restrict image, float *restrict 
 
     MPI_Status *status;
     printf("gets here \n");
-    //MPI_Sendrecv( firstRowSend , nx, MPI_FLOAT, 1, 0 , firstRowRecv , nx, MPI_FLOAT, 1, 0, MPI_COMM_WORLD, status);
+    MPI_Sendrecv( firstRowSend , nx, MPI_FLOAT, 1, 0 , firstRowRecv , nx, MPI_FLOAT, 1, 0, MPI_COMM_WORLD, status);
     printf("finish 2\n");
    
    
