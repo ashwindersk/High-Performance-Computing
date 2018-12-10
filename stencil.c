d864f3a58592db61545f9dccb3674bfc530a73e1
Ashwinder Khurana <ashwinderk@live.co.uk>
2018-12-10 10:34:04 +0000

    db deadlock

--- stencil.c
+++ stencil.c
@@ -103,7 +103,7 @@ void stencil(const int nx, const int ny, float *restrict image, float *restrict 
     MPI_Status *status;
 
     // MPI_Sendrecv(lastRowSend, nx, MPI_FLOAT, rank + 1, 0, lastRowRecv, nx, MPI_FLOAT, rank, 0, MPI_COMM_WORLD, status);
-    MPI_Send(lastRowSend, nx,  MPI_FLOAT,rank +1, 0, MPI_COMM_WORLD);
+    MPI_Send(lastRowSend, nx,  MPI_FLOAT,rank +1, MPI_ANY_TAG, MPI_COMM_WORLD);
     // MPI_Recv(lastRowRecv, nx, MPI_FLOAT,rank, 0,MPI_COMM_WORLD, status);
 
     printf("deadlock 0");
@@ -130,11 +130,11 @@ void stencil(const int nx, const int ny, float *restrict image, float *restrict 
     //Sending and receving data from each rank above and below in the image
     MPI_Status *status;
     printf("deadlock 1.1 and rank %d\n", rank);
-    MPI_Recv(firstRowRecv, nx,  MPI_FLOAT,rank, 0, MPI_COMM_WORLD, status);
+    MPI_Recv(firstRowRecv, nx,  MPI_FLOAT,rank, MPI_ANY_TAG, MPI_COMM_WORLD, status);
     // MPI_Send(firstRowSend, nx, MPI_FLOAT, rank -1, 0, MPI_COMM_WORLD);
 
 
-    MPI_Sendrecv( firstRowSend , nx, MPI_FLOAT, rank - 1, 0 , firstRowRecv , nx, MPI_FLOAT, rank, 0, MPI_COMM_WORLD, status);
+    //MPI_Sendrecv( firstRowSend , nx, MPI_FLOAT, rank - 1, 0 , firstRowRecv , nx, MPI_FLOAT, rank, 0, MPI_COMM_WORLD, status);
     //MPI_Sendrecv(firstRowSend, nx, MPI_FLOAT, 0, 0, firstRowRecv, nx, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, status);
 
     printf("deadlock 1.2 and rank %d\n", rank);
