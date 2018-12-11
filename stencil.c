49b51581e18594449a98bcfec6cd9c71fa86dd07
Ashwinder Khurana <ashwinderk@live.co.uk>
2018-12-11 01:35:45 +0000

    db

--- stencil.c
+++ stencil.c
@@ -117,8 +117,8 @@ void stencil(const int nx, const int ny, float *restrict image, float *restrict 
     for(int i = 0 ; i < ny; i++){
      for( int j =0 ; j< nx ; j++){    
        
-                   tmp_image[j+i*nx]  = image[j+i*nx] * 0.6;
-                   printf(" i: %d, j: %d",i,j);
+                   //tmp_image[j+i*nx]  = image[j+i*nx] * 0.6;
+                   printf(" i: %d, j: %d access value: %d\n",i,j, j+i*nx);
      // if(i>0)      {tmp_image[j+i*nx] += image[j+(i-1)*nx]*0.1;}
                    printf("hey\n");
       // if(i<ny-1)  tmp_image[j+i*nx] += image[j+(i+1)*nx] *0.1;
