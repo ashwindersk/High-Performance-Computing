f865f56e76aa0e161f20684e66164ba7f7018f87
Ashwinder Khurana <ashwinderk@live.co.uk>
2018-12-11 13:58:02 +0000

    db

--- stencil.c
+++ stencil.c
@@ -162,13 +162,13 @@ void stencil(const int nx, const int ny, float *restrict image, float *restrict 
    
     for(int i = 0 ; i < 64; i++){
      for( int j =0 ; j< 1024 ; j++){       
-                 //tmp_image[j+i*nx]  = image[j+i*nx] * 0.6;
-      // if(i>0)    tmp_image[j+i*nx] += image[j+(i-1)*nx]*0.1;
-      // if(i<ny-1) tmp_image[j+i*nx] += image[j+(i+1)*nx] *0.1;
+      tmp_image[j+i*nx]  = image[j+i*nx] * 0.6f;
+      // if(i>0)    tmp_image[j+i*nx] += image[j+(i-1)*nx]*0.1f;
+      // if(i<ny-1) tmp_image[j+i*nx] += image[j+(i+1)*nx] *0.1f;
     
-      // if(j>0)    tmp_image[j+i*nx] += image[j-1+i*nx]*0.1;
-      // if(j<nx-1) tmp_image[j+i*nx] += image[j+1 + i*nx]*0.1;
-      // if(i==0)   tmp_image[j+i*nx] += firstRowRecv[j]*0.1;
+      // if(j>0)    tmp_image[j+i*nx] += image[j-1+i*nx]*0.1f;
+      // if(j<nx-1) tmp_image[j+i*nx] += image[j+1 + i*nx]*0.1f;
+      // if(i==0)   tmp_image[j+i*nx] += firstRowRecv[j]*0.1f;
  
      }
     }
