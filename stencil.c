1d408d5d6349d8f48395b3591f51559ec165f815
Ashwinder Khurana <ashwinderk@live.co.uk>
2018-12-11 16:03:29 +0000

    db

--- stencil.c
+++ stencil.c
@@ -138,11 +138,12 @@ void stencil(const int nx, const int ny,  float *restrict image, float *restrict
 
     MPI_Status *status;
     //MPI_Sendrecv(lastRowSend, nx, MPI_FLOAT, rank -1, 0, lastRowRecv, nx, MPI_FLOAT, rank-1,0, MPI_COMM_WORLD, status);
-  MPI_Send(lastRowSend, nx ,MPI_FLOAT, rank-1, 0, MPI_COMM_WORLD );
-    MPI_Recv(lastRowRecv, nx ,MPI_FLOAT, rank-11, 0, MPI_COMM_WORLD,status );
+    MPI_Send(lastRowSend, nx ,MPI_FLOAT, rank-1, 0, MPI_COMM_WORLD );
+    MPI_Recv(lastRowRecv, nx ,MPI_FLOAT, rank-1, 0, MPI_COMM_WORLD,status );
 
 
   }
+  
 
 
  }
