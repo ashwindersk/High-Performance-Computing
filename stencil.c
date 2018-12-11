d5ff02c335744320b9728a12c89f74f961fe9fdd
Ashwinder Khurana <ashwinderk@live.co.uk>
2018-12-11 01:09:02 +0000

    db

--- stencil.c
+++ stencil.c
@@ -113,13 +113,13 @@ void stencil(const int nx, const int ny, float *restrict image, float *restrict 
     MPI_Recv(lastRowRecv, nx, MPI_FLOAT, rank+1, 0, MPI_COMM_WORLD, status);
 
     //Attempt 1 at implementing
-
+    printf("hello 1\n");
     for(int i = 0 ; i < ny; i++){
      for( int j =0 ; j< nx ; j++){       
                   tmp_image[j+i*nx]  = image[j+i*nx] * 0.6;
       if(i>0)     tmp_image[j+i*nx] += image[j+(i-1)*nx]*0.1;
       if(i<ny-1)  tmp_image[j+i*nx] += image[j+(i+1)*nx] *0.1;
-      printf("hello 1\n");
+      
       if(j>0)     tmp_image[j+i*nx] += image[j-1+i*nx]*0.1;
       if(j<nx-1)  tmp_image[j+i*nx] += image[j+1 + i*nx]*0.1;
       if(i==ny-1) tmp_image[j+i*nx] += lastRowRecv[j]*0.1;
@@ -159,7 +159,7 @@ void stencil(const int nx, const int ny, float *restrict image, float *restrict 
                  tmp_image[j+i*nx]  = image[j+i*nx] * 0.6;
       if(i>0)    tmp_image[j+i*nx] += image[j+(i-1)*nx]*0.1;
       if(i<ny-1) tmp_image[j+i*nx] += image[j+(i+1)*nx] *0.1;
-    printf("hey 1\n");
+    
       if(j>0)    tmp_image[j+i*nx] += image[j-1+i*nx]*0.1;
       if(j<nx-1) tmp_image[j+i*nx] += image[j+1 + i*nx]*0.1;
       if(i==0)   tmp_image[j+i*nx] += firstRowRecv[j]*0.1;
@@ -198,10 +198,11 @@ void stencil(const int nx, const int ny, float *restrict image, float *restrict 
     MPI_Recv(lastRowRecv, nx, MPI_FLOAT, rank+1, 0, MPI_COMM_WORLD, status);
     
     for(int i = 0 ; i < ny; i++){
-     for( int j =0 ; j< nx ; j++){       
+     for( int j =0 ; j< nx ; j++){   
+       printf("hi \n");    
                  tmp_image[j+i*nx]  = image[j+i*nx] * 0.6;
       if(i>0)    tmp_image[j+i*nx] += image[j+(i-1)*nx]*0.1;
-      printf("hi \n");
+      
       if(i<ny-1) tmp_image[j+i*nx] += image[j+(i+1)*nx] *0.1;
       if(j>0)    tmp_image[j+i*nx] += image[j-1+i*nx]*0.1;
       if(j<nx-1) tmp_image[j+i*nx] += image[j+1 + i*nx]*0.1;
