4c5ff6d50bd15e313ef15a7bb922189ec6c9d854
Ashwinder Khurana <ashwinderk@live.co.uk>
2018-12-11 17:58:04 +0000

    db

--- env.sh
+++ env.sh
@@ -11,8 +11,8 @@ module add tools/git-2.18.0
 module add languages/python-2.7.6
 module add languages/intel-compiler-16-u2
 module add openmpi/intel/64/1.6.5  
-#module add languages/gcc-7.1.0
+module add languages/gcc-7.1.0
 module add tools/intel_builds/tau-2.23.1-openmpi
-#module add openmpi/gcc/64/1.6.5
-#module add tools/gnu_builds/tau-2.23.1-openmpi
+module add openmpi/gcc/64/1.6.5
+module add tools/gnu_builds/tau-2.23.1-openmpi
 
