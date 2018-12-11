55c1e02811dbf7e57a04607f4e4c0452620f3cb6
Ashwinder Khurana <ashwinderk@live.co.uk>
2018-12-11 13:11:38 +0000

    db

--- stencil.c
+++ stencil.c
@@ -160,15 +160,15 @@ void stencil(const int nx, const int ny, float *restrict image, float *restrict 
     //  }
     // }
    
-    for(int i = 0 ; i < ny; i++){
-     for( int j =0 ; j< nx ; j++){       
-      //            tmp_image[j+i*nx]  = image[j+i*nx] * 0.6;
-      // if(i>0)    tmp_image[j+i*nx] += image[j+(i-1)*nx]*0.1;
-      // if(i<ny-1) tmp_image[j+i*nx] += image[j+(i+1)*nx] *0.1;
+    for(int i = 0 ; i < 64; i++){
+     for( int j =0 ; j< 1024 ; j++){       
+                 tmp_image[j+i*nx]  = image[j+i*nx] * 0.6;
+      if(i>0)    tmp_image[j+i*nx] += image[j+(i-1)*nx]*0.1;
+      if(i<ny-1) tmp_image[j+i*nx] += image[j+(i+1)*nx] *0.1;
     
-      // if(j>0)    tmp_image[j+i*nx] += image[j-1+i*nx]*0.1;
-      // if(j<nx-1) tmp_image[j+i*nx] += image[j+1 + i*nx]*0.1;
-      // if(i==0)   tmp_image[j+i*nx] += firstRowRecv[j]*0.1;
+      if(j>0)    tmp_image[j+i*nx] += image[j-1+i*nx]*0.1;
+      if(j<nx-1) tmp_image[j+i*nx] += image[j+1 + i*nx]*0.1;
+      if(i==0)   tmp_image[j+i*nx] += firstRowRecv[j]*0.1;
  
      }
     }
@@ -203,15 +203,15 @@ void stencil(const int nx, const int ny, float *restrict image, float *restrict 
     MPI_Send(lastRowSend, nx, MPI_FLOAT,rank+1, 0, MPI_COMM_WORLD );
     MPI_Recv(lastRowRecv, nx, MPI_FLOAT, rank+1, 0, MPI_COMM_WORLD, status);
     
-    for(int i = 0 ; i < ny; i++){
-     for( int j =0 ; j< nx ; j++){   
+    for(int i = 0 ; i < 64; i++){
+     for( int j =0 ; j< 1024 ; j++){   
        
-      //            tmp_image[j+i*nx]  = image[j+i*nx] * 0.6;
-      // if(i>0)    tmp_image[j+i*nx] += image[j+(i-1)*nx]*0.1;
+                 tmp_image[j+i*nx]  = image[j+i*nx] * 0.6;
+      if(i>0)    tmp_image[j+i*nx] += image[j+(i-1)*nx]*0.1;
       
-      // if(i<ny-1) tmp_image[j+i*nx] += image[j+(i+1)*nx] *0.1;
-      // if(j>0)    tmp_image[j+i*nx] += image[j-1+i*nx]*0.1;
-      // if(j<nx-1) tmp_image[j+i*nx] += image[j+1 + i*nx]*0.1;
+      if(i<ny-1) tmp_image[j+i*nx] += image[j+(i+1)*nx] *0.1;
+      if(j>0)    tmp_image[j+i*nx] += image[j-1+i*nx]*0.1;
+      if(j<nx-1) tmp_image[j+i*nx] += image[j+1 + i*nx]*0.1;
      }
     }
     // for(int i = 0 ; i < ny; i++){
