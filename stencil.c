e4da8177897f08c7d6e5574f7963d82f534eb8e0
Ashwinder Khurana <ashwinderk@live.co.uk>
2018-12-11 01:59:04 +0000

    db

--- stencil.c
+++ stencil.c
@@ -113,14 +113,12 @@ void stencil(const int nx, const int ny, float *restrict image, float *restrict 
     MPI_Recv(lastRowRecv, nx, MPI_FLOAT, rank+1, 0, MPI_COMM_WORLD, status);
 
     //Attempt 1 at implementing
-    
+    printf("value in image %f  value in temp %f\n", image[65535], tmp_image[65535]);
     for(int i = 0 ; i < ny; i++){
      for( int j =0 ; j< nx ; j++){    
        
                    //tmp_image[j+i*nx]  = image[j+i*nx] * 0.6;
-                  if(i==ny-1 && j==nx-1){
-                   printf(" i: %d, j: %d access value: %d\n",i,j, j+i*nx);
-                  } 
+                 
      // if(i>0)      {tmp_image[j+i*nx] += image[j+(i-1)*nx]*0.1;}
                    
       // if(i<ny-1)  tmp_image[j+i*nx] += image[j+(i+1)*nx] *0.1;
