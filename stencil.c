6a0bfaeeb409759204789a69f1083951094f5c48
Ashwinder Khurana <ashwinderk@live.co.uk>
2018-12-11 17:53:35 +0000

    db

--- stencil.c
+++ stencil.c
@@ -157,23 +157,7 @@ void stencil(const int nx, const int ny, float *restrict image, float *restrict 
     MPI_Recv(firstRowRecv, nx , MPI_FLOAT, rank-1, 0, MPI_COMM_WORLD, status);
     MPI_Send(firstRowSend, nx , MPI_FLOAT, rank-1, 0, MPI_COMM_WORLD );
 
-    // for(int i = 0 ; i < ny; i++){
-    //  for( int j =0 ; j< nx ; j++){       
-    //                                    tmp_image[j+i*nx] = image[j+i*nx] * 0.6;
-    //  if(i<1 && j>0 && j<nx-1)          tmp_image[j+i*nx] += firstRowRecv[j] + image[(j+1) + i*nx]*0.1 + 0.1*image[j + (i+1)*nx] +  0.1*image[(j-1) + i*nx];
-    //  else if(j<1)                      tmp_image[j+i*nx] += 0.1*image[j+1+i*nx] + 0.1*image[j+(i-1)*nx] + 0.1*image[j+(i+1)*nx];
-    //  else if(j>nx-2)                   tmp_image[j+i*nx] += 0.1*image[j-1+i*nx] + 0.1*image[j+(i-1)*nx] + 0.1*image[j+(i+1)*nx];
-    //  else if(i>ny-2 && j>0 && j<nx-1 ) tmp_image[j+i*nx] += 0.1*image[j + (i-1)*nx] + 0.1*image[j-1 + i*nx] + 0.1*image[j+1 + i*nx];
-    //  else {                            tmp_image[j+i*nx] += 0.1*image[j+1+i*nx] + 0.1*image[j-1 + i*nx] + 0.1*image[j + (i-1)*nx] + 0.1*0.1*image[j + (i+1)*nx]; } 
-    //  }
-    // }
    
-
-    // for(int i =0  ; i< 64 ; i++){
-    //   for( int j= 0 ; j< 1024 ; j++){
-    //     printf("value %f\n", image[j+i*nx]);
-    //   }
-    // }
 
     for(int i = 0 ; i < 64; i++){
      for( int j =0 ; j< 1024 ; j++){       
